6899a68021dac0e8c3f66f0795ad4746436d936a
Cristian F. Tovar <cristhiam.tovar@mail.escuelaing.edu.co>
2020-02-17 19:45:30 -0500

    Reblending this mess

--- src/components/HamburgerButton.jsx
+++ src/components/HamburgerButton.jsx
@@ -10,31 +10,33 @@ const HamburgerButtonWrapper = styled.div`
   & span {
     width: 25px;
     height: 3px;
-    background-color: ${(props) => (props.color ? props.color : '#3f3f3f')};
+    background-color: ${props => (props.color ? props.color : '#3f3f3f')};
     margin: 5px auto;
     transition: all 0.3s ease-in-out;
     display: block;
     cursor: pointer;
   }
   & span:nth-child(2) {
-    opacity: ${(props) => (props.isActive ? '0' : '1')};
+    opacity: ${props => (props.isActive ? '0' : '1')};
   }
   & span:nth-child(1) {
-    transform: ${(props) => (props.isActive ? 'translateY(8px) rotate(45deg)' : '')};
+    transform: ${props =>
+      props.isActive ? 'translateY(8px) rotate(45deg)' : ''};
   }
   & span:nth-child(3) {
-    transform: ${(props) => (props.isActive ? 'translateY(-8px) rotate(-45deg)' : '')};
+    transform: ${props =>
+      props.isActive ? 'translateY(-8px) rotate(-45deg)' : ''};
   }
   @media (min-width: 1024px) {
     display: none;
   }
 `;
 
-const HamburgerButton = (props) => {
+const HamburgerButton = props => {
   const [isActive, setIsActive] = useState(false);
   const { color, id, className } = props;
 
-  const handleHamburgerClick = (event) => {
+  const handleHamburgerClick = event => {
     event.preventDefault();
     setIsActive(!isActive);
   };
